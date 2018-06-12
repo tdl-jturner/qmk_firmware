f2d3e034c9395e323c298570ba299906ce9afc82
Josh Turner <turner.josh@gmail.com>
2018-06-12 08:18:45 -0700

    Added back horizontal numbers

--- keyboards/planck/keymaps/tdl-jturner/keymap.c
+++ keyboards/planck/keymaps/tdl-jturner/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //Num Keys
  [_LYNUM] =
    {
-     {KC_LYDEF,________,________,________,________,________,___XX___,KC_7    ,KC_8    ,KC_9    ,KC_MINUS,________},
+     {KC_GRV  ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,  KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  ,KC_MINUS},
      {________,________,________,________,________,________,___XX___,KC_4    ,KC_5    ,KC_6    ,KC_PPLS ,KC_PSLS },
      {________,________,________,________,________,________,___XX___,KC_1    ,KC_2    ,KC_3    ,KC_PENT ,KC_PAST },
      {________,________,________,________,________,________,________,KC_0    ,KC_0    ,KC_PDOT ,KC_PEQL ,___XX___}
